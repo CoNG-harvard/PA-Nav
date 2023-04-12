4b7b82c900fc5a2585a1565e659d621002377b1a
Tianpeng <tzhang@g.harvard.edu>
2023-04-12 17:10:34 -0400

    MA conflict check finished.

--- panav/PBS/conflict.py
+++ panav/PBS/conflict.py
@@ -1,4 +1,27 @@
 import numpy as np
+
+def MA_plan_conflict(times,plans):
+    for i in range(len(plans)):
+        t,x=times[i],plans[i]
+        for j in range(i+1,len(plans)):
+            tp,xp = times[j],plans[j]
+            if path_conflict(t,x,tp,xp,bloating_r,bloating_r):
+                return True
+    return False
+
+def path_conflict(t,x,tp,xp,r,rp):
+    for k in range(len(t)-1):
+        ta = t[k:k+2]
+        pa = x[:,k:k+2]
+        for m in range(len(tp)-1):
+            tb = tp[m:m+2]
+            pb = xp[:,m:m+2]
+            if seg_conflict(ta,pa,tb,pb,r,rp):
+                # print('ta',ta,'pa',pa)
+                # print('tb',tb,'pb',pb)
+                return True
+    return False
+
 def seg_conflict(ta,pa,tb,pb,ra,rb):
     ''' 
         Determine if two space-time segments have conflict.
@@ -34,7 +57,8 @@ def seg_conflict(ta,pa,tb,pb,ra,rb):
     t_star = -u.dot(v)/v.dot(v)
     
     if lb<=t_star<=ub:
-        return True  # The center lines of two segments intersect.
+        return np.linalg.norm(u + t_star*v)<= ra+rb  # The minimal distance is attained when t=t_star.
     else:
         return np.linalg.norm(u + lb * v)<= ra+rb \
             or np.linalg.norm(u + ub * v)<= ra+rb 
+            # The minimal distance is attained when t=lb or ub.
