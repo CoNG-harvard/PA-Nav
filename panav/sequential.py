c4ed1d22b5c2ac2a11973becd1ec0cc669dc6c7a
Tianpeng <tzhangam@gmail.com>
2024-08-09 11:32:48 -0400

    Keep optimizing the calling structure for MILP main

--- panav/sequential.py
+++ panav/sequential.py
@@ -1,48 +1,30 @@
 
 from time import time
-import signal
  
-def timeout_handler(signum,frame):
-    print("Algorithm timeout")
-    raise TimeoutError("Timeout")
-def sequential_planning(solver,env,vmax,bloating_r,TIMEOUT = 120,lazy = True,return_cumu_times = False):
+def sequential_planning(solver,env,vmax,bloating_r,TIMEOUT = 120,lazy = True):
     '''
         Sequential MAMP using a given solver.
     '''
-    signal.signal(signal.SIGALRM, timeout_handler)
-    signal.alarm(TIMEOUT)
-
+  
     t0 = time()
-
-    cumulative_times = []
     continuous_plans = []
 
-    try:        
-        for i in range(len(env.starts)):
-            print('Planning for agent {}/{}'.format(i,len(env.starts)))
-            start = env.starts[i,:]
-            goal = env.goals[i,:]
-            
-            sol = solver(env,start,goal,vmax = vmax, bloating_r =  bloating_r)
-            p = sol.plan(obstacle_trajectories = continuous_plans,lazy = lazy)    
+    for i in range(len(env.starts)):
+        print('Planning for agent {}/{}'.format(i,len(env.starts)))
+        start = env.starts[i,:]
+        goal = env.goals[i,:]
+        
+        sol = solver(env,start,goal,vmax = vmax, bloating_r =  bloating_r)
+        p = sol.plan(obstacle_trajectories = continuous_plans,lazy = lazy)    
 
-            t_now = time()-t0
-            if p is None:
-                return None
-            if t_now>TIMEOUT or p is None: # Stop early if the run time has exceeded TIMEOUT
-                break
+        t_now = time()-t0
+        if p is None:
+            return None
+        if t_now>TIMEOUT: # Stop early if the run time has exceeded TIMEOUT
+            break
 
-            cumulative_times.append(t_now)
-            continuous_plans.append(p)
-    except TimeoutError:
-            print("Timeout")
-            signal.alarm(0)
-
-    
-    if return_cumu_times:
-        n_max = min(len(continuous_plans),len(cumulative_times))
-        return continuous_plans[:n_max], cumulative_times[:n_max]
-    
+        continuous_plans.append(p)
+        
     return continuous_plans
 
 import numpy as np
